e29ba7614888bc89e5b795b0196f139138f81369
Bertin-fap <francois.bertin7@wanadoo.fr>
2024-12-24 11:01:09 +0100

    modified make_etiquette.py

--- make_etiquettes.py
+++ make_etiquettes.py
@@ -1,5 +1,7 @@
 import ctg.ctggui as ctgg
 import ctg.ctgfuncts as ctg
+
+import os
 import random
 from pathlib import Path
 import pandas as pd
@@ -11,34 +13,13 @@ from docx2pdf import convert
 from PyPDF4 import PdfFileMerger
 
 def make_sample(n_sample):
-    n_sample = 150
-    year = 2024
-    hebergement = [('St julien de Montdenis','Hôtel Lancheton'),
-                   ('St Michel de Maurienne','Hôtel  le Galibier'),
-                   ('St Michel de Maurienne','Hôtel  Varcin'),
-                   ('St Michel de Maurienne','Hôtel le Savoy'),
-                   ('St Michel de Maurienne','Hôtel camping le  Marintan'),
-                   ('St Michel de Maurienne','Lycée'),
-                   ('St Michel de Maurienne','Collège'),
-                   ('VALMEINIER 1500 et 1800','le grand Fourchon'),
-                   ('VALMEINIER 1500 et 1800','Hôtel les Carretes'),
-                   ('VALLOIRE-les Granges','Hôtel le Tatami'),
-                   ('VALLOIRE (centre village)','Hôtel de la Poste'),
-                   ('VALLOIRE (centre village)','Hôtel les Mélèzes'),
-                   ('VALLOIRE (centre village)','Hôtel Christiana'),
-                   ('VALLOIRE (centre village)',"Hôtel L'Aiguille Noire"),
-                   ('VALLOIRE (centre village)',"Hôtel le Centre"),
-                   ('VALLOIRE (centre village)',"Hôtel La Maison Rapin"),
-                   ('VALLOIRE (centre village)',"Le Grand Hôtel"),
-                   ('VALLOIRE - Les Verneys',"Hôtel le Relais du Galibier"),
-                   ('VALLOIRE - Les Verneys',"Hôtel le Crêt Rond"),
-                   ('VALLOIRE - Les Verneys',"Gîte les Réaux"),
-                   ('VALLOIRE - Les Verneys',"Pulka"),
-                   ('VALLOIRE - Les Verneys',"Le Val D'Or"),
-                   ('Valmeinier 1500',"L'Arména"),
-                   ]
-    ctg_path = r"c:\users\franc\CTG\SORTIES"
-    effectif = ctg.EffectifCtg(year,ctg_path)
+
+    
+    URL = r"I:\Mon Drive\DRIVE DU CTG\3-PROJETS DU CTG\2-BRA 2025-PDU\5-HEBERGEMENT - JAR\hebergement.xlsx"
+    hebergement_df = pd.read_excel(URL)
+    hebergement = list(zip(hebergement_df['Ville'],hebergement_df['Hôtel']))
+    ctg_path = Path.home() / r"CTG\SORTIES"
+    effectif = ctg.EffectifCtg(2024,ctg_path)
     sample = effectif.effectif.sample(n=n_sample)[['Nom','Prénom','N° Portable']]
     sample['ville-hotel'] = [random.choice(hebergement) for _ in range(n_sample)]
     sample[['ville', 'hotel']] = pd.DataFrame(sample['ville-hotel'].tolist(), index=sample.index)
@@ -46,11 +27,12 @@ def make_sample(n_sample):
     sample = sample.drop(['ville-hotel','Nom','Prénom'],axis=1)
     sample["Dossard"] = [random.randint(1,800) for _ in range(n_sample)]
     sample = sample.fillna('inconnu')
+    sample = sample.sort_values(by=['Dossard'], ascending=True)
     return sample
 
 def make_etiquettes(sample):
-    template_path_docx = Path.home() / r"CTG\BRA-BRO-BG\BRA\DATA\EtiquetteBagage_BRA.docx"
-    output_file = Path.home() / r"Temp/essai.docx"
+    
+    template_path_docx = Path.home() / r"CTG\BRA-BRO-BG\BRA\DATA\transport\template_EtiquetteBagage_BRA.docx"
     long = 10
     
     year = 2025
@@ -71,25 +53,40 @@ def make_etiquettes(sample):
                 'tel': sample['N° Portable'].tolist()[i_dep:len(sample)],
                 'ville': sample['ville'].tolist()[i_dep:len(sample)],
                 'lieu': sample['hotel'].tolist()[i_dep:len(sample)],}
-        
+
+        print(f)
+        print()
         doc = DocxTemplate(template_path_docx) 
         doc.render(f)
-        output_file = Path.home() / Path(r"Temp/essai"+str(idx)+".docx")
+    
+        output_file = Path.home() / Path(r"CTG\BRA-BRO-BG\BRA\DATA\transport\essai"+str(idx)+".docx")
         doc.save(output_file)
-        file_list.append(Path.home() / Path(r"Temp/essai"+str(idx)+".pdf"))
+        convert(output_file) # Creates a pdf file
+        output_file = Path.home() / Path(r"CTG\BRA-BRO-BG\BRA\DATA\transport\essai"+str(idx)+".pdf")
+        file_list.append(output_file)
         idx += 1
         
-        convert(output_file) # Creates a pdf file
+        
         
     
     merger = PdfFileMerger(strict=False)
     
     for pdf in file_list:
         merger.append(str(pdf))
-    
-    merger.write(str(Path.home() / Path(r"Temp/merge.pdf")))
-    merger.close()
 
-n_sample = 105
+    output_file = Path.home() / Path(r"CTG\BRA-BRO-BG\BRA\DATA\transport\merge.pdf")
+    merger.write(str(output_file))
+    merger.close()
+    
+    for pdf in file_list:
+        os.remove(pdf)
+        file = Path(pdf).stem+".docx"
+        path = Path(os.path.dirname(pdf)) / file
+        os.remove(path)
+
+
+n_sample = 27
 sample = make_sample(n_sample)
+sample
 make_etiquettes(sample)
+print("SUCCESSFULLY ENDED")
